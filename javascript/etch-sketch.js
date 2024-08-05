a7e68ae630687bde5eecfb2024f004c1402d8a9c
Tanvir Bhamber <77294257+BigBhamber@users.noreply.github.com>
2024-08-05 17:25:54 -0400

    added initial grid setup

--- javascript/etch-sketch.js
+++ javascript/etch-sketch.js
@@ -1,1 +1,17 @@
-console.log("hello World");
+
+
+const initSize = 16;
+
+function genGrid(size) {
+    const container = document.querySelector("#container");
+
+    let numLoops = size*size;
+    for (let i =0;i<numLoops; i++){
+        const divbox = document.createElement("div");
+        divbox.classList.add("grid")
+        container.appendChild(divbox);
+    }
+
+}
+
+genGrid(initSize)
