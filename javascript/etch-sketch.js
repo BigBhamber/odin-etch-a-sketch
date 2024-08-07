c3af8052f6175d46d86f6555cdf55cf5913ed6b3
Tanvir Bhamber <77294257+BigBhamber@users.noreply.github.com>
2024-08-07 01:45:27 -0400

    added logic for resetting and generating new board. need to fix issue with board not properly retaining square shape, or having equal number of squares per line

--- javascript/etch-sketch.js
+++ javascript/etch-sketch.js
@@ -18,8 +18,6 @@ function genGrid(size) {
     }
 
 }
-
-genGrid(initSize)
 function setColor(){
 
     //this function sets the color upon hover on a color.
@@ -29,4 +27,51 @@ function setColor(){
     let backgroundColor = "rgb("+x+","+y+","+z+")";
     this.style.backgroundColor = backgroundColor
 
+}
+
+genGrid(initSize)
+
+const resetBtn = document.querySelector("#reset");
+
+resetBtn.addEventListener("click",()=> {
+    let size = prompt("Select grid size (2-100)")
+    validateInput(parseInt(size));
+});
+function clearBoard()
+{
+    const board = document.querySelector("#container")
+    let child = board.lastElementChild
+    while(child)
+    {
+        board.removeChild(child);
+        child = board.lastElementChild;
+    }
+}
+function validateInput(size)
+{
+    //this function validates users input
+    if (Number.isInteger(size))
+    {
+        //check if number is within 2 and 100
+        if (size>1 && size<100)
+        {
+            //call initialize board function and delete old board function
+            clearBoard();
+            genGrid(size);
+
+        }
+        else{
+            // generate board with 16 size again (put error message in console log)
+            console.log("Invalid input will generate with base case")
+            clearBoard()
+            genGrid(16);
+            
+        }
+    }
+    else{
+        //invalid case with generate with 16
+        console.log("Invalid input will generate with base case");
+        clearBoard()
+        genGrid(16);
+    }
 }
