ecfdddcd7c9e8be7b676c6e1432c1c09d1bd8e98
Tanvir Bhamber <77294257+BigBhamber@users.noreply.github.com>
2024-08-07 00:18:36 -0400

    changed logic for generating grid

--- javascript/etch-sketch.js
+++ javascript/etch-sketch.js
@@ -5,11 +5,13 @@ const initSize = 16;
 function genGrid(size) {
     const container = document.querySelector("#container");
 
-    let numLoops = size*size;
-    for (let i =0;i<numLoops; i++){
+    
+    for (let i = 0;i<size; i++){
+        for (let x = 0;x<size;x++){
         const divbox = document.createElement("div");
         divbox.classList.add("grid")
         container.appendChild(divbox);
+        }
     }
 
 }
