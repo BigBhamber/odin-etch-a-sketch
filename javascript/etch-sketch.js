0fcc0ed731b23e5f9601981899f3edb79334a2ce
Tanvir Bhamber <77294257+BigBhamber@users.noreply.github.com>
2024-08-07 00:44:55 -0400

    added implementation for generating random color on hover event

--- javascript/etch-sketch.js
+++ javascript/etch-sketch.js
@@ -3,6 +3,7 @@
 const initSize = 16;
 
 function genGrid(size) {
+    //this function generates the board and populates it with squares with hover event listeners.
     const container = document.querySelector("#container");
 
     
@@ -10,6 +11,8 @@ function genGrid(size) {
         for (let x = 0;x<size;x++){
         const divbox = document.createElement("div");
         divbox.classList.add("grid")
+        divbox.style.backgroundColor = "violet";
+        divbox.addEventListener("mouseover",setColor)
         container.appendChild(divbox);
         }
     }
@@ -17,3 +20,13 @@ function genGrid(size) {
 }
 
 genGrid(initSize)
+function setColor(){
+
+    //this function sets the color upon hover on a color.
+    let x = Math.floor(Math.random()*256);
+    let y = Math.floor(Math.random()*256);
+    let z = Math.floor(Math.random()*256);
+    let backgroundColor = "rgb("+x+","+y+","+z+")";
+    this.style.backgroundColor = backgroundColor
+
+}
